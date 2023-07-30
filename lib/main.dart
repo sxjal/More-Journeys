d3a9dcd4c19676a88c1ce7fcc47b5fb9d5ed2881
sajal <sajalsahu8@gmail.com>
2023-07-31 00:34:54 +0400

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:morejourneys/cubit/app_cubit_logics.dart';
+import 'package:morejourneys/cubit/app_cubits.dart';
+import 'package:morejourneys/pages/detail_pages/cubit/store_page_info_cubits.dart';
+import 'package:morejourneys/pages/detail_pages/detail_page.dart';
+import 'package:morejourneys/pages/navpages/main_page.dart';
+import 'package:morejourneys/pages/welcome_page.dart';
+import 'package:morejourneys/services/data_services.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            iconTheme: IconThemeData(
+              color:Colors.black
+            ),
+            elevation: 0,
+            backgroundColor: Colors.white
+          )
+        ),
+        home: MultiBlocProvider(
+          providers: [
+            BlocProvider<AppCubits>(
+              create: (context) => AppCubits(
+                data: DataServices(),
+              ),
+            ),
+            BlocProvider<StorePageInfoCubits>(
+              create: (context) => StorePageInfoCubits(),
+            )
+          ],
+          child: const AppCubitLogics(),
+        ));
+  }
 }
