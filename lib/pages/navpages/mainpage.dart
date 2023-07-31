25da3e4a425e88078ba1397e894e07d6b1e11d0a
sajal <sajalsahu8@gmail.com>
2023-07-31 04:22:44 +0400

    added pages

--- lib/pages/navpages/mainpage.dart
+++ lib/pages/navpages/mainpage.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:morejourneys/pages/navpages/baritempage.dart';
+import 'package:morejourneys/pages/navpages/mypage.dart';
+import 'package:morejourneys/pages/navpages/searchpage.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List pages = const [
+    BarItemPage(),
+    SearchPage(),
+    MyPage(),
+  ];
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-        items: [],
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.apps), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: "Bar"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "My"),
+        ],
       ),
     );
   }
