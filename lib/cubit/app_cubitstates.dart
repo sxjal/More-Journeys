098c2f3cc7d2d1b70053999dbb1963ca583c1a9e
sajal <sajalsahu8@gmail.com>
2023-08-01 01:13:15 +0400

    comments

--- lib/cubit/app_cubitstates.dart
+++ lib/cubit/app_cubitstates.dart
@@ -4,10 +4,15 @@ abstract class AppCubitState extends Equatable {
   const AppCubitState();
 }
 
+//every cubut class must have initial state,
+//and the initial state must extend the cubit state
 class InitialState extends AppCubitState {
   @override
   List<Object?> get props => [];
 }
+
+//ones thats done, we need to trigger our main welcomepage which
+//will be done by creating a new state called WelcomeState
 
 class WelcomeState extends AppCubitState {
   @override
