91bfb7c945279f4d2efd917d37e3171c6463cc72
sajal <sajalsahu8@gmail.com>
2023-08-01 01:57:01 +0400

    api req

--- lib/services/dataservices.dart
+++ lib/services/dataservices.dart
@@ -1,3 +1,4 @@
+import "dart:convert";
 import "dart:io";
 
 import "package:http/http.dart" as http;
@@ -5,8 +6,18 @@ import "package:http/http.dart" as http;
 class DataService {
   String baseurl = "http://mark.bslmeiyu.com/api";
   void getInfo() async {
-    var apiUrl = '/getPlaces';
+    var apiUrl = '/getplaces';
 
     final response = await http.get(Uri.parse(baseurl + apiUrl));
+
+    try {
+      if (response.statusCode == 200) {
+        List<dynamic> list = json.decode(response.body);
+        
+      }
+    } catch (e) {
+      print(response.statusCode);
+      print(e);
+    }
   }
 }
